67c4910e4104937a7bb7eef223fe3adc82a96336
Sheargrub <69406976+Sheargrub@users.noreply.github.com>
2026-09-03 11:50:20 -0500

    Remove and relocate vestigal GitHub login content

--- src/theme/NavbarItem/ComponentTypes.tsx
+++
@@ -1,7 +0,0 @@
-import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
-import NavbarGithubAuth from './NavbarGithubAuth';
-
-export default {
-  ...ComponentTypes,
-  'custom-githubAuth': NavbarGithubAuth,
-};
